61cf71429f64f97fc1ba87e79e488134a83ac404
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-04-29 12:01:28 +0300

    map,player,sprites,animation logic

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,9 @@
-class Player {
-  constructor({ CollisionBlocks = [] }) {
+class Player extends Sprite {
+  constructor({ CollisionBlocks = [], imageSrc, frameRate }) {
+    super({ imageSrc, frameRate });
     this.position = {
-      x: 100,
-      y: 100,
+      x: 200,
+      y: 200,
     };
 
     this.velocity = {
@@ -10,55 +11,83 @@ class Player {
       y: 0,
     };
 
-    this.width = 100;
-    this.height = 100;
     this.sides = {
       bottom: this.position.y + this.height,
     };
     this.gravity = 1;
     this.collisionBlocks = collisionBlocks;
-    console.log(this.collisionBlocks);
   }
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
+  // draw() {
+  //   c.fillStyle = "red";
+  //   c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  // }
   update() {
+    // c.fillStyle = "rgba(0,0,255,0.5)";
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height);
     this.position.x += this.velocity.x;
-    //check horizontal collision
-    for (let i = 0; i < this.collisionBlocks.lenght; i++) {
+    this.checkForHorizontalCollisions();
+    this.applyGravity();
+    this.checkForVerticalCollisions();
+  }
+  checkForHorizontalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i];
 
-      //if collision exs.
+      // if a collision exists
       if (
         this.position.x <= collisionBlock.position.x + collisionBlock.width &&
         this.position.x + this.width >= collisionBlock.position.x &&
-        this.position.y + this.height >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
         this.position.y <= collisionBlock.position.y + collisionBlock.height
       ) {
-        {
-          //if collision of x axis to left
-          if (this.velocity.x < -1) {
-            this.position.x =
-              collisionBlock.position.x + collisionBlock.width + 0.01;
-            break;
-          }
-          if (this.velocity.x > 1) {
-            this.position.x = collisionBlock.position.x - this.width - 0.01;
-            break;
-          }
+        // collision on x axis going to the left
+        if (this.velocity.x < -0) {
+          const offset = this.position.x - this.position.x;
+          this.position.x =
+            collisionBlock.position.x + collisionBlock.width - offset + 0.01;
+          break;
+        }
+
+        if (this.velocity.x > 0) {
+          const offset = this.position.x - this.position.x + this.width;
+          this.position.x = collisionBlock.position.x - offset - 0.01;
+          break;
         }
       }
     }
+  }
 
+  applyGravity() {
+    this.velocity.y += this.gravity;
     this.position.y += this.velocity.y;
-    this.sides.bottom = this.position.y + this.height;
+  }
 
-    if (this.sides.bottom + this.velocity.y < canvas.height) {
-      this.position.y++;
-      this.velocity.y += this.gravity;
-    } else {
-      this.velocity.y = 0;
+  checkForVerticalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+
+      // if a collision exists
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          const offset = this.position.y - this.position.y;
+          this.position.y =
+            collisionBlock.position.y + collisionBlock.height - offset + 0.01;
+          break;
+        }
+
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          const offset = this.position.y - this.position.y + this.height;
+          this.position.y = collisionBlock.position.y - offset - 0.01;
+          break;
+        }
+      }
     }
   }
 }
